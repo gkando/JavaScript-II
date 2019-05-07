0b98a3fa15aa6afa6828ae58cf6297b8de0d7e9d
George Kando <gkando@gmail.com>
2019-05-07 13:22:08 -0500

    1st commit - partially complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -25,26 +25,78 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
+/*  (1)  */
+
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+    // getLength passes the length of the array into the callback.
+    return cb(arr.length);
+  }
+
+  function printLength(length) {
+    console.log("There are " + length + " items in the array.")
+  }
+
+  getLength(items, printLength);
+
+
+/*  (2)  */
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr, arr.length-1); 
 }
+
+  function lastItem(arr, i) {
+    console.log("The last item in the array is " + arr[i] + ".");
+  }
+  
+  last(items, lastItem)
+
+  
+/*  (3)  */
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  z = x + y
+  cb(x, y, z);
 }
 
-function multiplyNums(x, y, cb) {
+  function numsSummed(x, y, z) {
+    console.log("The sum of " + x + " & " + y + " is " + z + "!");
+  }
+  
+  sumNums(9, 10, numsSummed);
+
+
+/*  (4)  */
+
+ function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  z = x * y
+  cb(x, y, z);
 }
+
+  function numsMultiplied(x, y, z) {
+    console.log("The product of " + x + " & " + y + " is " + z + "!");
+  }
+  
+  multiplyNums(9, 10, numsMultiplied);
+
+
+/*  (5)  */
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  var results = Boolean(list.map(function(e) { return e; }).indexOf(item));
+  cb(results);
 }
+
+  function containsDecision(results) {
+    console.log(results);
+  }
+
+contains("Gum", items, containsDecision);
 
 /* STRETCH PROBLEM */
 
